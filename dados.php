8f6bc645d1b2254f530f4383b36b9208f0da45e3
EduardoDanelli <eduardodanelli@gmail.com>
2025-02-14 22:56:50 -0300

    feat: Criação dos dados, livros e melhora no index

---
+++ dados.php
@@ -0,0 +1,9 @@
+<?php
+
+$livros = [
+    ['id' => 1, 'titulo' => "Senhor do Anéis", 'autor' => "Tolkien", 'descricao' => "Destruir o anel"],
+    ['id' => 2, 'titulo' => "Guerra dos Tronos", 'autor' => "Martin", 'descricao' => "Sentar no trono"],
+    ['id' => 3, 'titulo' => "Alquimista", 'autor' => "Coelho", 'descricao' => "Encontrar o tesouro"],
+    ['id' => 4, 'titulo' => "Meditações", 'autor' => "Aurélio", 'descricao' => "Encontrar a si mesmo"],
+    ['id' => 5, 'titulo' => "Memórias Póstumas", 'autor' => "Assis", 'descricao' => "Memórias de um morto"],
+];
